9285eef87988fe0de1c4c4939d6690895b91d76f
kduval11 <143556513+kduval11@users.noreply.github.com>
2023-09-12 18:10:10 -0400

    Update pratt_schedule1.py

--- week_2/pratt_schedule1.py
+++ week_2/pratt_schedule1.py
@@ -1,6 +1,6 @@
 all_classes = ['Human-Information Behavior','Research Mthds/Law Lit','Strategic Leadership','Conservation and Preservation','Info Services & Resources', 'Information Arch/Inter Design','Information Professions','Acad Libraries and Scholarly','Usability Theory & Practice','Mgmt of Archives/Sp Collection','Government Info Sources','Library Media Centers','Mgmt of Archives/Sp Collection','Information Science Research']
+print(all_classes[2])
 print(all_classes[3])
-print(all_classes[4])
 
 
 
