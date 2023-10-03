de65c5d752fc061cc3db77f9134420bfaa6d332a
kduval11 <143556513+kduval11@users.noreply.github.com>
2023-10-03 16:16:33 -0400

    Update week_4.py

--- week_4/week_4.py
+++ week_4/week_4.py
@@ -8,13 +8,13 @@ with open('Documents/workspace/Pratt664/week_4/Artworks.csv') as artwork_file:
         artist_nationality = art['Nationality'].split(" ")
         for nationality in artist_nationality:
             if nation_file.get(nationality) is None:
-                with open(f"res/{nationality}.csv", "w") as nationality_file:
+                with open(f"Documents/workspace/Pratt664/week_4/res/{nationality}.csv", "w") as nationality_file:
                     nat_dict_writer = csv.DictWriter(nationality_file, processed_csv.fieldnames)
                     nat_dict_writer.writeheader()
                     nat_dict_writer.writerow(art)
                     nation_file[nationality] = True
             else:
-                with open(f"res/{nationality}.csv", "a") as nationality_file:
+                with open(f"Documents/workspace/Pratt664/week_4/res/{nationality}.csv", "a") as nationality_file:
                     nat_dict_writer = csv.DictWriter(nationality_file, processed_csv.fieldnames)
                     nat_dict_writer.writerow(art)
 
